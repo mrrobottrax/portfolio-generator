56620ddc05e68dd0fdaab3eee4db76ba1e978091
Andrew <andrewjhoult@gmail.com>
2024-04-30 21:58:03 -0400

    Add template project pages

--- project-card-loader.js
+++ project-card-loader.js
@@ -1,35 +1,58 @@
 window.onload = populateProjects;
 
+class ProjectCard {
+	constructor(title, description, link, imageUrl) {
+		this.title = title;
+		this.description = description;
+		this.link = link;
+		this.imageUrl = imageUrl;
+	}
+
+	getHTML() {
+		return `
+		<a href="${this.link}">
+			<div class="project-card">
+				<img src="${this.imageUrl}" alt="Project thumbnail"/>
+				<div>
+					<strong>${this.title}</strong>
+					<p>${this.description}</p>
+				</div>
+			</div>
+		</a>
+		`
+	}
+}
+
 function populateProjects() {
 	const collabCards = [
 		new ProjectCard(
 			"BEAT 'Em Up",
 			"A rythm-based beat 'em up. Fight rival musically themed gangs to reclaim you turf in Funky Town. Made as a semester long project with a group of 5.",
-			"projects/beat-em-up/page.html",
+			"project-page.html?project-id=beat-em-up",
 			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/main/projects/beat-em-up/images/thumbnail.png",
 		),
 		new ProjectCard(
 			"Ice To Beat You",
 			"A mix of physical and digital. Battle it out in a snowball fight against the neighborhood hooligans using real fake snowballs! Made during a 4 day game jame with a group of 6.",
-			"projects/snow/page.html",
+			"project-page.html?project-id=snow",
 			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/main/projects/snow/images/thumbnail.png",
 		),
 		new ProjectCard(
 			"Obscurum",
 			"A sound intensive horror game where you can only see by using your trusty poking stick. Made during a 4 day game jam with a group of 5.",
-			"projects/sound/page.html",
+			"project-page.html?project-id=obscurum",
 			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/main/projects/sound/images/thumbnail.png",
 		),
 		new ProjectCard(
 			"Faking News",
 			"Make up headlines, find photos for the cover, and broadcast your stories to your friends. The result of a 48 hour game jam with a group of 5.",
-			"projects/faking-news/page.html",
+			"project-page.html?project-id=news",
 			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/main/projects/faking-news/images/thumbnail.png",
 		),
 		new ProjectCard(
 			"O2",
 			"Delve into the depth of a mysterious planet, but make sure to manage your limited oxygen. Made during a 48 hour game jam with a group of 3.",
-			"projects/o2/page.html",
+			"project-page.html?project-id=o2",
 			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/main/projects/o2/images/thumbnail.png",
 		),
 	];
@@ -38,19 +61,19 @@ function populateProjects() {
 		new ProjectCard(
 			"WebGL Based Engine",
 			"A 3D engine written in TypeScript with multiplayer support, node.js server hosting, and a web-based level editor.",
-			"projects/web3d/page.html",
+			"project-page.html?project-id=webgl",
 			"projects/web3d/images/thumbnail.png",
 		),
 		new ProjectCard(
 			"Raytracer",
 			"A simple realtime raytracing engine.",
-			"projects/raytrace/page.html",
+			"project-page.html?project-id=raytrace",
 			"projects/raytrace/images/thumbnail.png",
 		),
 		new ProjectCard(
 			"3D Physics",
 			"A simple 3D physics engine.",
-			"projects/physics/page.html",
+			"project-page.html?project-id=physics",
 			"projects/physics/images/thumbnail.png",
 		),
 	];
---
+++ project-page-loader.js
@@ -0,0 +1,45 @@
+const params = new URLSearchParams(location.search);
+const projectId = params.get("project-id");
+
+class ProjectData {
+	constructor(name, description, images) {
+		this.name = name;
+		this.description = description;
+		this.images = images;
+	}
+
+	getHTML() {
+		return `
+			<h1>${this.name}</h1>
+			<p>${this.description}</p>
+		`;
+	}
+}
+
+function getProjectData() {
+	switch (projectId) {
+		case "beat-em-up":
+			return new ProjectData("BEAT 'Em Up", "This is a semester long group project", []);
+		case "news":
+			return new ProjectData("Faking News", "This is a semester long group project", []);
+		case "snow":
+			return new ProjectData("Ice To Beat You", "This is a semester long group project", []);
+		case "obscurum":
+			return new ProjectData("Obscurum", "This is a semester long group project", []);
+		case "o2":
+			return new ProjectData("O2", "This is a semester long group project", []);
+		case "webgl":
+			return new ProjectData("WebGL Based Engine", "This is a semester long group project", []);
+		case "physics":
+			return new ProjectData("3D Physics", "This is a semester long group project", []);
+		case "raytrace":
+			return new ProjectData("Raytracer", "This is a semester long group project", []);
+	}
+}
+
+window.onload = () => {
+	const project = getProjectData();
+	if (project) {
+		document.getElementById("project-body").innerHTML = project.getHTML();
+	}
+}
