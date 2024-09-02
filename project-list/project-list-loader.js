f95943366b376c9d1268aad4f3a4646bfa553043
Andrew <mrrobottrax@gmail.com>
2024-09-02 17:46:32 -0400

    Update wgl thumbnail 2

--- project-list/project-list-loader.js
+++ project-list/project-list-loader.js
@@ -64,7 +64,7 @@ function populateProjects() {
 			"WebGL Engine",
 			"A 3D engine written in TypeScript with multiplayer support, node.js server hosting, and a web-based level editor.",
 			"webgl",
-			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/mainprojects/webgl/images/thumbnail.png",
+			"https://media.githubusercontent.com/media/andrewhoult/andrewhoult.github.io/main/projects/webgl/images/thumbnail.png",
 		),
 		// new ProjectCard(
 		// 	"Raytracer",
